f1ad0e5ad971880d81480e159168d9a50731c113
zak <codedpills@gmail.com>
2021-03-09 13:35:34 +0000

    open socket connection and emit messages

--- index.js
+++ index.js
@@ -1,19 +1,30 @@
-const path = require('path');
-const http = require('http');
+const path = require("path");
+const http = require("http");
 const express = require("express");
-const socketio = require('socket.io');
+const socketio = require("socket.io");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 // set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // run when client connects
-io.on('connection', socket => {
-    console.log('New WS connection...');
-})
+io.on("connection", (socket) => {
+  console.log("New WS connection...");
+
+  // welcome current user
+  socket.emit("message", "Welcome to Chatchord");
+
+  // broadcast when a user connects
+  socket.broadcast.emit("message", "A new user has joined the chat");
+
+  //runs when a client disconnects
+  socket.on("disconnect", () => {
+    io.emit("message", "A user has left the chat");
+  });
+});
 
 const PORT = 4000 || process.env.PORT;
 
