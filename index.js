539d1c199af5e2bd56b1ab792f91f2da1981f148
zak <codedpills@gmail.com>
2021-03-09 12:58:42 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const http = require('http');
+const express = require("express");
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// run when client connects
+io.on('connection', socket => {
+    console.log('New WS connection...');
+})
+
+const PORT = 4000 || process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
