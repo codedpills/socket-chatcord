06bf61be4be5ae8fa9cc75d3f310e1ccdda1087b
zak <codedpills@gmail.com>
2021-03-09 14:59:29 +0000

    format chat message

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,21 +11,26 @@ const io = socketio(server);
 // set static folder
 app.use(express.static(path.join(__dirname, "public")));
 
+const botName = "Chatchord";
+
 // run when client connects
 io.on("connection", (socket) => {
   // welcome current user
-  socket.emit("message", "Welcome to Chatchord");
+  socket.emit("message", formatMessage(botName, "Welcome to Chatchord"));
 
   // broadcast when a user connects
-  socket.broadcast.emit("message", "A new user has joined the chat");
+  socket.broadcast.emit(
+    "message",
+    formatMessage(botName, "A new user has joined the chat")
+  );
 
   //runs when a client disconnects
   socket.on("disconnect", () => {
-    io.emit("message", "A user has left the chat");
+    io.emit("message", formatMessage(botName, "A user has left the chat"));
   });
 
   socket.on("chatMessage", (msg) => {
-    io.emit("message", msg);
+    io.emit("message", formatMessage("USER", msg));
   });
 });
 
